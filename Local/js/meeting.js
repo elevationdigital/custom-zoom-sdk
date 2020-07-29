806a801321389c2f87130647499b02a18c889d84
Simon Norman <zhymon.norman@gmail.com>
2020-07-29 16:44:01 -0400

    styling and such

--- Local/js/meeting.js
+++ Local/js/meeting.js
@@ -1,4 +1,6 @@
-import { ZoomMtg } from "@zoomus/websdk";
+import { ZoomMtg } from "./zoom.js";
+import './app.scss';
+
 const testTool = window.testTool;
 // get meeting args from url
 const tmpArgs = testTool.parseQuery();
@@ -33,7 +35,7 @@ const meetingConfig = {
     }
   })(),
   lang: tmpArgs.lang,
-  signature: tmpArgs.signature || "",
+  signature: "dVp3SlFHZndRX0dzTWdrZ1kyVWdSUS43NzQ4ODIxMjM5LjE1OTYwMzE5MzQ3MjQuMC5WMGZpbXhrWncvdlZyaWIyb0VHWGZVSFRqYUNFS1dKbGQwaTRDUzRwZlRvPQ==" || "",
   china: tmpArgs.china === "1",
 };
 
@@ -63,6 +65,9 @@ function beginJoin(signature) {
           ZoomMtg.getAttendeeslist({});
           ZoomMtg.getCurrentUser({
             success: function (res) {
+              document.addEventListener('DOMContentLoaded', function() {
+                document.getElementById('wc-container-left').classList.add('show-participants');
+              }, false);
               console.log("success getCurrentUser", res.result.currentUser);
             },
           });
@@ -79,3 +84,4 @@ function beginJoin(signature) {
 }
 
 beginJoin(meetingConfig.signature);
+
